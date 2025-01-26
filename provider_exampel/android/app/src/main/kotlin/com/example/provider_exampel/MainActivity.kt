ffa2710a1e0fcf067ec835f8f9d12f004213240a
hebah <hebahalsoub20@gmail.com>
2025-01-26 13:42:50 +0300

    add project

---
+++ provider_exampel/android/app/src/main/kotlin/com/example/provider_exampel/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.provider_exampel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ provider_exampel/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+
+import 'controller/name_swap_controller.dart';
+import 'screen/swapname.dart';
+
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      child: const MainApp(),
+      create: (context) => SwapNameCoontroller(),
+    ),
+  );
+}
+
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SwapName(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
